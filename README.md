b37968611df0cd01bd8773968197d5366d020f76
Aymon2016 <mdaiman2016@gmail.com>
2023-01-27 17:39:17 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# amazone_clone_project-ecomer_website_fullStack_portfolio-project-2" 
---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5007;
+const cookieParser = require("cookie-parser");
+const DefaultData = require("./defaultData");
+require("./db/connection");
+const router = require("./Routes/router");
+const Products = require("./models/productsSchema");
+const jwt = require("jsonwebtoken");
+const cors=require('cors')
+const path= require('path')
+
+
+// middleware
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(router);
+
+
+
+// if(process.env.NODE_ENV == "production"){
+//     app.use(express.static("client/build"));
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
+
+app.listen(port,()=>{
+    console.log(`your server is running on port ${port} `);
+});
+DefaultData()
+
